731c7f82a8c64d8319e28c94692b1182846cf3f1
Steven Petryk <me@stevenpetryk.com>
2017-04-09 18:00:21 -0400

    Send job to elastic transcoder

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
+const restler = require('restler')
+
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3({ region: process.env.AWS_REGION })
 const transcoder = new AWS.ElasticTranscoder({ region: process.env.AWS_REGION })
 
 const stream = require('stream')
-
 const binaryServer = require('binaryjs').BinaryServer
 const wav = require('wav')
 
 const server = binaryServer({ port: 9001 })
 
-function uploaderFactory (streamId) {
+function uploaderFactory (streamId, token) {
   var pass = new stream.PassThrough()
 
   const key = `streams/${streamId}/audio.wav`
@@ -18,15 +19,14 @@ function uploaderFactory (streamId) {
   const options = { partSize: 5 * 1024 * 1024, queueSize: 3, computeChecksums: true }
   s3.upload(params, options, (err, data) => {
     if (!err) {
-      console.log('Uploaded.')
-      doTranscode(key, streamId, JSON.parse(data.ETag))
+      doTranscode(key, streamId, JSON.parse(data.ETag), token)
     }
   })
 
   return pass
 }
 
-function doTranscode (inputKey, streamId, hash) {
+function doTranscode (inputKey, streamId, hash, token) {
   const newKey = `${hash}.mp3`
 
   var params = {
@@ -46,32 +46,45 @@ function doTranscode (inputKey, streamId, hash) {
     }]
   }
 
-  console.log('Beginning transcoding...', params)
-
   transcoder.createJob(params, (err, data) => {
     if (err) {
-      console.log(err, err.stack)
+      console.error(err, err.stack)
       return
     }
 
-    console.log('Done!', data)
+    doUpdate(streamId, `streams/${streamId}/${newKey}`, token)
   })
+}
+
+function doUpdate (streamId, key, token) {
+  const payload = {
+    stream: {
+      audio_s3_key: key
+    }
+  }
+
+  restler.patchJson(
+    `${process.env.STREAMR_API_ENDPOINT}/streams/${streamId}`,
+    payload,
+    { accessToken: token }
+  )
+    .on('success', (data, response) => console.log(data, response))
+    .on('fail', (data, response) => console.log(data, response))
 }
 
 server.on('connection', (client) => {
   var uploader = null
   var wavWriter = null
 
-  wavWriter = new wav.Writer({
-    channels: 1,
-    sampleRate: 48000,
-    bitDepth: 16
-  })
-
   client.on('stream', (bitStream, meta) => {
     const streamId = meta.streamId
 
-    uploader = uploader || uploaderFactory(streamId)
+    uploader = uploader || uploaderFactory(streamId, meta.authToken)
+    wavWriter = wavWriter || new wav.Writer({
+      channels: 1,
+      sampleRate: meta.sampleRate,
+      bitDepth: 16
+    })
 
     bitStream.pipe(wavWriter).pipe(uploader)
   })
